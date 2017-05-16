75e5e7b942a0b5c97fd40961b22727a63cf09a11
marcelaorellana <marcelapaz.1985@gmail.com>
2017-05-15 22:27:26 -0400

    cada validación separada por funciones

--- js/main.js
+++ js/main.js
@@ -1,22 +1,55 @@
+
 function validateForm(){
 	var nombre = document.getElementById("name").value;
 	var apellido = document.getElementById("lastname").value;
 	var correo = document.getElementById("input-email").value;
 	var contrasena = document.getElementById("input-password").value;
 	var seleccion = document.getElementsByTagName('select')[0].selectedIndex;
+	var social = document.getElementById("input-social").value;
 
+
+	/*validarNombre(nombre);
+	
+	validarApellido(apellido);
+
+	validarCorreo(correo);
+
+	validarContrasena(contrasena);
+
+	validarSeleccion(seleccion);*/
+
+
+
+	if(validarNombre(nombre) && validarApellido(apellido) && validarCorreo(correo) && validarContrasena(contrasena) && validarSeleccion(seleccion)){
+		alert("Tus Datos Validamente Emitidos son: "+ "\nnombre: " + nombre + " " + apellido + "\nCorreo: " + correo + 
+			"\nContraseña: " + contrasena + "\nBicicleta: " + document.getElementsByTagName('select')[0].value + 
+			"\nRed Social: @" + social);
+		
+	}
+		
+		
+
+		return true;
+}
+
+function validarNombre(nombre){
+	
 	if (nombre == "" || nombre.charAt(0).toUpperCase() != nombre.charAt(0) || /^[a-zA-Z]+$/.test(nombre) == false){
 		var errorNombre=document.createElement('div');
 		errorNombre.setAttribute("id","nombre1");
 		errorNombre.setAttribute("class","inputs");
 		var spanNombre = document.createElement('span');
-		spanNombre.innerHTML= 'Escribe tu nombre con Mayúscula';                   
 		errorNombre.appendChild(spanNombre);
-		document.getElementById("name").parentNode.appendChild(errorNombre);
-
-		return false;
+		document.getElementById("name").parentNode.appendChild(errorNombre);	
+		spanNombre.innerHTML= 'Escribe tu nombre con Mayúscula';  
+		return false;             	
 	}
-	else if (apellido == "" || apellido.charAt(0).toUpperCase() != apellido.charAt(0) || /^[a-zA-Z]+$/.test(apellido) == false){
+	
+	return true;
+}
+
+function validarApellido(apellido){
+	if (apellido == "" || apellido.charAt(0).toUpperCase() != apellido.charAt(0) || /^[a-zA-Z]+$/.test(apellido) == false){
 		var errorApellido=document.createElement('div');
 		errorApellido.setAttribute("id","apellido1");
 		errorApellido.setAttribute("class","inputs");
@@ -27,7 +60,11 @@ function validateForm(){
 
 		return false;
 	}
-	else if(correo.indexOf("@") == -1){
+	return true;
+}
+
+function validarCorreo(correo){
+	if(correo.indexOf("@") == -1){
 		var errorCorreo=document.createElement('div');
 		errorCorreo.setAttribute("id","correo1");
 		errorCorreo.setAttribute("class","inputs");
@@ -35,8 +72,13 @@ function validateForm(){
 		spanCorreo.innerHTML= 'Correo No Válido';                   
 		errorCorreo.appendChild(spanCorreo);
 		document.getElementById("input-email").parentNode.appendChild(errorCorreo);
+		return false;
 	}
-	else if(contrasena.length < 6 || contrasena == "password" || contrasena == "123456" || contrasena == "098765"){
+	return true;
+}
+
+function validarContrasena(contrasena){
+	if(contrasena.length < 6 || contrasena == "password" || contrasena == "123456" || contrasena == "098765"){
 		var errorContrasena=document.createElement('div');
 		errorContrasena.setAttribute("id","contrasena1");
 		errorContrasena.setAttribute("class","inputs");
@@ -44,8 +86,13 @@ function validateForm(){
 		spanContrasena.innerHTML= 'Contraseña no válida';                   
 		errorContrasena.appendChild(spanContrasena);
 		document.getElementById("input-password").parentNode.appendChild(errorContrasena);
+		return false;
 	}
-	else if(seleccion == 0){
+	return true;
+}
+
+function validarSeleccion(seleccion){
+	if(seleccion == 0){
 		var errorSeleccion=document.createElement('div');
 		errorSeleccion.setAttribute("id","seleccion1");
 		errorSeleccion.setAttribute("class","inputs");
@@ -53,11 +100,7 @@ function validateForm(){
 		spanSeleccion.innerHTML= 'Elija un tipo de bicicleta';                   
 		errorSeleccion.appendChild(spanSeleccion);
 		document.getElementsByTagName('select')[0].parentNode.appendChild(errorSeleccion);
+		return false;
 	}
-
-
-		//alert("nombre: " + nombre + " " + apellido + "\nCorreo: " + correo + 
-		//	"\nContraseña: " + contrasena);
-
-		return true;
+	return true;
 }
